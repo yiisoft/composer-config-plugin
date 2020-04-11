2ca57d085d78ffa84324d2b3550b99951dabf5d8
Dmitriy Derepko <xepozz@list.ru>
2020-04-11 21:44:19 +0300

    Removed var_dump

--- tests/Integration/Tests/PluginTestCase.php
+++ tests/Integration/Tests/PluginTestCase.php
@@ -12,11 +12,9 @@ abstract class PluginTestCase extends TestCase
     private const BASE_DIRECTORY = __DIR__ . '/../Environment';
     private const OUTPUT_DIRECTORY = self::BASE_DIRECTORY . '/vendor/yiisoft/composer-config-plugin-output';
 
-    final protected function setUp()
+    final protected function setUp(): void
     {
-        var_dump(self::OUTPUT_DIRECTORY);
         Builder::rebuild(self::OUTPUT_DIRECTORY);
-
     }
 
     protected function getParam(string $name)
