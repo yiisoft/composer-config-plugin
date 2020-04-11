6bea7cd792f72b07eaf2981ca9fb1f3b80b78ce0
Dmitriy Derepko <xepozz@list.ru>
2020-04-11 21:20:48 +0300

    Fix stub paths

--- tests/Integration/runtime/__files.php
+++ tests/Integration/runtime/__files.php
@@ -1,13 +1,11 @@
 <?php
 
-$baseDir = dirname(__DIR__, 6);
-
-defined('COMPOSER_CONFIG_PLUGIN_BASEDIR') or define('COMPOSER_CONFIG_PLUGIN_BASEDIR', $baseDir);
+$baseDir = dirname(__DIR__) . '/config/';
 
 return [
     'dotenv' => [],
     'defines' => [],
-    'params' => [$baseDir . '/PhpstormProjects/yiisoft/yii-web/config/params.php'],
-    'web' => [$baseDir . '/PhpstormProjects/yiisoft/yii-web/config/web.php'],
-    'tests' => [$baseDir . '/PhpstormProjects/yiisoft/yii-web/config/web.php'],
+    'params' => [$baseDir . '/params.php'],
+    'web' => [$baseDir . '/web.php'],
+    'tests' => [$baseDir . '/web.php'],
 ];
