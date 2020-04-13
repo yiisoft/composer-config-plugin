3e709a1fc1523a44f837c01b67e089970cc2a8cb
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2020-04-13 19:21:45 +0300

    Call static method correctly

--- src/Builder.php
+++ src/Builder.php
@@ -55,7 +55,7 @@ class Builder
     {
         $this->outputDir = $outputDir
             ? static::buildAbsPath($this->getBaseDir(), $outputDir)
-            : $this->findOutputDir();
+            : static::findOutputDir();
     }
 
     public static function rebuild(string $outputDir = null): void
