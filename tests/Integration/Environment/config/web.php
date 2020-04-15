e67d315c18219ad176906bea360f1895be625d14
Dmitriy Derepko <xepozz@list.ru>
2020-04-15 21:14:20 +0300

    Add helpful phpdoc

--- tests/Integration/Environment/config/web.php
+++ tests/Integration/Environment/config/web.php
@@ -2,6 +2,10 @@
 
 declare(strict_types=1);
 
+/**
+ * @var $params array
+ */
+
 return [
     \Environment\Serializer\SerializerInterface::class => \Environment\Serializer\PhpSerializer::class,
     'params' => $params,
