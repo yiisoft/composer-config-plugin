d139fa96306b66ada28a6c4d8aff4c9d32125199
Dmytro Naumenko <d.naumenko.a@gmail.com>
2020-04-17 12:50:01 +0300

    Implemented constants testing (#20)

---
+++ tests/Integration/Environment/config/constants.php
@@ -0,0 +1,3 @@
+<?php
+
+defined('TEST_CONSTANT') or define('TEST_CONSTANT', 'a constant value defined in config/constants.php');
--- tests/Integration/Environment/repositories/first-vendor/first-package/config/params.php
+++ tests/Integration/Environment/repositories/first-vendor/first-package/config/params.php
@@ -4,4 +4,5 @@ declare(strict_types=1);
 
 return [
     'first-vendor/first-package' => true,
+    'constant_from_vendor' => TEST_CONSTANT_FROM_VENDOR,
 ];
