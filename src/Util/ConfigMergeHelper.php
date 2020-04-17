241589d7ee84d80f6e3bebd2e1343b873cd0a35c
Dmitriy Derepko <xepozz@list.ru>
2020-04-17 21:48:02 +0300

    Refactoring

--- src/Util/ConfigMergeHelper.php
+++ src/Util/ConfigMergeHelper.php
@@ -7,7 +7,6 @@ namespace Yiisoft\Composer\Config\Util;
 use Yiisoft\Arrays\ReplaceArrayValue;
 use Yiisoft\Arrays\UnsetArrayValue;
 
-use function func_get_args;
 use function in_array;
 use function is_array;
 use function is_int;
@@ -17,39 +16,49 @@ final class ConfigMergeHelper
     /**
      * Merges two or more arrays into one recursively.
      *
+     * @param array[] $arrays
      * @return array the merged array
      */
-    public static function mergeConfig(): array
+    public static function mergeConfig(array ...$arrays): array
     {
-        $args = func_get_args();
-        $res = array_shift($args) ?: [];
-        foreach ($args as $items) {
+        $result = array_shift($arrays) ?: [];
+        foreach ($arrays as $items) {
             if (!is_array($items)) {
                 continue;
             }
-            foreach ($items as $k => $v) {
-                if ($v instanceof UnsetArrayValue) {
-                    unset($res[$k]);
-                } elseif ($v instanceof ReplaceArrayValue) {
-                    $res[$k] = $v->value;
-                } elseif (is_int($k)) {
+            foreach ($items as $key => $value) {
+                if ($value instanceof UnsetArrayValue) {
+                    unset($result[$key]);
+                    continue;
+                }
+
+                if ($value instanceof ReplaceArrayValue) {
+                    $result[$key] = $value->value;
+                    continue;
+                }
+
+                if (is_int($key)) {
                     /// XXX skip repeated values
-                    if (in_array($v, $res, true)) {
+                    if (in_array($value, $result, true)) {
                         continue;
                     }
-                    if (isset($res[$k])) {
-                        $res[] = $v;
-                    } else {
-                        $res[$k] = $v;
+
+                    if (array_key_exists($key, $result)) {
+                        $result[] = $value;
                     }
-                } elseif (is_array($v) && isset($res[$k]) && is_array($res[$k])) {
-                    $res[$k] = self::mergeConfig($res[$k], $v);
-                } else {
-                    $res[$k] = $v;
+
+                    continue;
                 }
+
+                if (is_array($value) && array_key_exists($key, $result) && is_array($result[$key])) {
+                    $result[$key] = self::mergeConfig($result[$key], $value);
+                    continue;
+                }
+
+                $result[$key] = $value;
             }
         }
 
-        return $res;
+        return $result;
     }
 }
