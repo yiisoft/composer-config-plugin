e29bb2ef17ca19755eaace32bac18890993bdfc4
Dmitriy Derepko <xepozz@list.ru>
2020-04-19 10:25:26 +0300

    Fix #21

--- src/Configs/Envs.php
+++ src/Configs/Envs.php
@@ -2,11 +2,25 @@
 
 namespace Yiisoft\Composer\Config\Configs;
 
+use Yiisoft\Composer\Config\Utils\Helper;
+
 /**
  * DotEnv class represents output configuration file with ENV values.
  */
 class Envs extends Config
 {
+    protected function writeFile(string $path, array $data): void
+    {
+        $envs = Helper::exportVar($data);
+
+        $content = <<<PHP
+<?php
+return {$envs};
+PHP;
+
+        $this->contentWriter->write($path, $content . PHP_EOL);
+    }
+
     public function envsRequired(): bool
     {
         return false;
