ec656a8447384e05dd8f95d37a0ec0f4e965461a
Dmitriy Derepko <xepozz@list.ru>
2020-04-19 10:25:26 +0300

    Fix #21

---
+++ src/Utils/PathHelper.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Composer\Config\Utils;
+
+final class PathHelper
+{
+    /**
+     * PHP implementation of {@see realpath()} method returns `false` when file or directory does not exist.
+     * This method prevents this behavior.
+     *
+     * @param string $path
+     * @return string
+     */
+    public static function realpath(string $path): string
+    {
+        $parts = explode('/', self::normalize($path));
+        $out = [];
+        foreach ($parts as $part) {
+            if ($part === '.') {
+                continue;
+            }
+            if ($part === '..') {
+                array_pop($out);
+                continue;
+            }
+            $out[] = $part;
+        }
+
+        return implode('/', $out);
+    }
+
+    public static function normalize(string $path): string
+    {
+        $search = ['//', '\\\\', '\\'];
+        $replace = ['/', '\\', '/'];
+        while (($processedPath = str_replace($search, $replace, $path)) !== $path) {
+            $path = $processedPath;
+        }
+
+        return rtrim($path, '/');
+    }
+}
