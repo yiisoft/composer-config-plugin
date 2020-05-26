dfb92802faf41042ff86de7b4ec0db85ca80b942
Dmitriy Derepko <xepozz@list.ru>
2020-05-26 21:34:15 +0300

    test

--- tests/Integration/ComposerUpdateHook.php
+++ tests/Integration/ComposerUpdateHook.php
@@ -14,7 +14,7 @@ final class ComposerUpdateHook implements BeforeFirstTestHook
         $command = sprintf(
             '%s && %s',
             $this->cwdToEnvironment(),
-            '[ -d vendor ] && composer dump || composer update ' . $this->suppressLogs(),
+            '[ -d vendor ] && composer dump || composer update '  ,
         );
         $this->exec($command);
     }
