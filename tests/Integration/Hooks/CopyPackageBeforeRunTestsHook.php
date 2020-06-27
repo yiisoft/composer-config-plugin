c2fc487e045e2c924e5a6e588fdca2c8cddc5520
Dmitriy Derepko <xepozz@list.ru>
2020-06-27 16:11:53 +0300

    Fix bug

--- tests/Integration/Hooks/CopyPackageBeforeRunTestsHook.php
+++ tests/Integration/Hooks/CopyPackageBeforeRunTestsHook.php
@@ -14,7 +14,7 @@ final class CopyPackageBeforeRunTestsHook implements BeforeFirstTestHook
     public function executeBeforeFirstTest(): void
     {
         $composerConfigPluginDirectory = dirname(__DIR__) . '/Packages/yiisoft/';
-        $pluginCurrentVersionDirectory = dirname(__DIR__, 4);
+        $pluginCurrentVersionDirectory = dirname(__DIR__, 3);
 
         if (file_exists($composerConfigPluginDirectory)) {
             $this->removeDirectoryRecursive($composerConfigPluginDirectory);
