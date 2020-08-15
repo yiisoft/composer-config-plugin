184f1f673f358a18b96e3d3c451f3ad36ec38932
Alexander Makarov <sam@rmcreative.ru>
2020-08-15 18:54:32 +0300

    Add tests

--- tests/Integration/Environment/config/web.php
+++ tests/Integration/Environment/config/web.php
@@ -6,7 +6,10 @@ declare(strict_types=1);
  * @var $params array
  */
 
+use Yiisoft\Composer\Config\Builder;
+
 return [
     \Environment\Serializer\SerializerInterface::class => \Environment\Serializer\PhpSerializer::class,
     'params' => $params,
+    'required' => Builder::require('required')
 ];
